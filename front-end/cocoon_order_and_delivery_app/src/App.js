0f08680d912778212ca7f1599b8203b0a5d00ac5
Aklile-Yilma <aklileyilma@gmail.com>
2023-01-02 02:57:09 +0300

    intial folder structure

--- front-end/cocoon_order_and_delivery_app/src/App.js
+++ front-end/cocoon_order_and_delivery_app/src/App.js
@@ -1,7 +1,14 @@
+// import custom css
 import "./App.css";
+// import tailwind
+import "./index.css";
 
 function App() {
-  return <div className="App">Cocoon App</div>;
+  return (
+    <div className="App text-3xl font-bold text-cyan-300 underline">
+      Cocoon App
+    </div>
+  );
 }
 
 export default App;
