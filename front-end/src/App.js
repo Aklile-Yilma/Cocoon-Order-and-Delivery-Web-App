a8fb45f049fee19795b875876fc42d4cfa8b083d
Brownoak <helanii93@gmail.com>
2023-01-14 01:29:39 -0800

    creating header component

--- front-end/src/App.js
+++ front-end/src/App.js
@@ -3,10 +3,12 @@ import "./app.css";
 import "./index.css";
 import Cart from "./components/Cart";
 import ProductDetail from "./components/ProductDetail";
+import Header from "./components/header/Header";
 
 function App() {
   return (
-    <div>
+    <div className="flex flex-col ">
+      <Header/>
       <ProductDetail />
     </div>
   );
