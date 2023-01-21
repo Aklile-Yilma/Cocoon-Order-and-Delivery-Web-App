39525e4cb24fa9a634be580f61f2121069b7991b
Brownoak <helanii93@gmail.com>
2023-01-20 22:59:57 -0800

    styling the header

--- front-end/src/components/header/Header.js
+++ front-end/src/components/header/Header.js
@@ -1,33 +1,58 @@
-import {FaBars, FaShoppingCart, FaSearch} from "react-icons/fa"
+import {FaBars, FaShoppingCart} from "react-icons/fa"
 import image1 from "../../assets/images/cocoonLogo.png";
 import image2 from "../../assets/images/profile.png";
+import { useState, useEffect } from "react";
 
 const Header =()=>{
-   
+    const[open, setOpen]= useState(false)
+    const showNavbar = ()=>{
+        setOpen(!open)
+
+    }
+    const [isScrolled, setIsScrolled] = useState(false)
+    useEffect (() => {
+        const handleScroll = () => {
+            if (window.scrollY > 0) {
+            setIsScrolled(true)
+            }else {
+            setIsScrolled(false)
+            }
+        }
+            window.addEventListener('scroll', handleScroll)
+            return () => {
+                window.removeEventListener('scroll', handleScroll)
+            
+            }
+            }, []) 
 
     return(
-        <div className="flex fixed justify-around w-screen pt-12.8"  >
-            <div className="flex">
-                <img src={image1} className="w-36 "/>
-                <div >
-                    <ul className=" pt-12 pr-3 pl-20 list-none w-auto " >
-                        <li className="inline align-middle  mr-5"><a href="" className="no-underline text-black font-bold ">Menu </a></li>
-                        <li className="inline align-middle mr-5"><a href="" className="no-underline text-black font-bold">About</a></li>
-                        <li className="inline align-middle mr-5"><a href="" className="no-underline text-black font-bold">Contact</a></li>
-
-                    </ul>
+            <div className={` ${ isScrolled && "bg-stone-200"} h-20 fixed flex justify-around w-screen md:flex fixed `} >
+                <div className="shrink-0 w-30 md:flex">
+                    <img src={image1} className="flex-shrink-0 truncate block m-auto flex-row overflow-hidden flex-col w-28"/>
                 </div>
-            </div>
-            <div className="flex items-center pl-3.5">
+                    <div className=" justify-between pl-3.5 block  flex-col">
+                        <ul className={` sm:  ${open ? " text-center": "hidden"} md:flex items-center pt-7 pr-3 pl-14 list-none xl:w-auto lg:flex `} >
+                            <li className=" align-middle  mr-5"><a href="#" className="no-underline text-black font-bold ">Menu </a></li>
+                            <li className=" align-middle mr-5"><a href="#" className="no-underline text-black font-bold">About</a></li>
+                            <li className=" align-middle mr-5"><a href="#" className="no-underline text-black font-bold">Contact</a></li>
 
-                <button className=" mr-5 bg-main py-2  w-36 text-xs flex gap-6 items-center justify-center  font-bold  text-white hover:ring-gray-500 rounded-2xl  r  ">Log out</button>
-                <button className="mr-5 " href=" "><FaShoppingCart size={25}/></button>
-                <img src={image2} className="w-20 mr-5 "></img>
+                        </ul> 
+                    </div> 
                 
-            </div>
+                <div className="flex justify-between md:flex items-center pl-3.5 ">
+                    <button className={` mr-5  flex  ${open ?"block": "hidden"}  md:block bg-main py-2 w-36  text-xs gap-6 items-center justify-center  font-bold  text-white hover:ring-gray-500 rounded-2xl r block lg:visible `}>Log out</button>
+                    <FaBars className="cursor-pointer md:hidden  flex mr-5 "size={25} onClick={showNavbar} />
+                    <button className="mr-5 pl-2" href=" "><FaShoppingCart size={25}/></button>
+                    <img src={image2} className=" w-20 mr-5 " alt="profileIcon"></img>
+
+                    
+                </div>
+
+
+                
             
-           
-        </div>
+            </div>
+        
         
     )
 }
