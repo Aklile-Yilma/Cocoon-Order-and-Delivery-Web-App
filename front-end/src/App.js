da0cb64c307f79ba138be83762a5eb4908bb02e4
kidussintayehu <kidussintayehu60@gmail.com>
2023-01-21 21:12:03 +0300

    Revert "Add home page"
    This reverts commit 8a8d5e129425840fb4cacb827258dc336908da34.

--- front-end/src/App.js
+++ front-end/src/App.js
@@ -1,17 +1,17 @@
 import Menu from "./components/Menu";
-import "./App.css";
+import "./app.css";
 import "./index.css";
 import Cart from "./components/Cart";
 import ProductDetail from "./components/ProductDetail";
 import Header from "./components/header/Header";
-// import AboutCard from "./components/card/AboutCard";
+import AboutCard from "./components/card/AboutCard";
 
 function App() {
   return (
     <div className="flex flex-col ">
       <Header/>
       {/* <ProductDetail /> */}
-      {/* <AboutCard/> */}
+      <AboutCard/>
 
     </div>
   );
