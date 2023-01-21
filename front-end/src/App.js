c6e0216d510375caa917621e778a7b92cceb0f8f
kidussintayehu <kidussintayehu60@gmail.com>
2023-01-21 21:20:58 +0300

    restructure package places

--- front-end/src/App.js
+++ front-end/src/App.js
@@ -1,5 +1,5 @@
 import Menu from "./components/Menu";
-import "./app.css";
+import "./App.css";
 import "./index.css";
 import Cart from "./components/Cart";
 import ProductDetail from "./components/ProductDetail";
